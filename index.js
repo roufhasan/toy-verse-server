2436ed6219ea34dee85d2bdf258f941e776d17e6
roufhasane <roufhasan5@gmail.comm>
2023-05-21 18:48:53 +0600

    post method created

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("toyVerse");
+    const toysCollection = db.collection("toys");
+
+    app.post("/addToy", async (req, res) => {
+      const body = req.body;
+      const result = await toysCollection.insertOne(body);
+      console.log(body);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
